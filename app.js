33c3b1411a8a4e7b53de3719e069f0536cae8f37
vlah02 <m.vlahovic02@hotmail.com>
2025-07-14 02:56:49 +0200

    session secret change even though this is not meant for production but i have friends that like to judge my code

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: 'CHANGE_THIS_SECRET',
+  secret: 'marija_smarac',
   resave: false,
   saveUninitialized: false
 }));
